1bb0c7561cfcd1a019c9d1c30a4d70c1b9b14a6d
Astrolitterbox <opit@hardcore.lt>
2013-04-11 11:55:42 +0200

    reads axis ratios, 2D fit

--- get_results.py
+++ get_results.py
@@ -10,10 +10,10 @@ def split_errors(result_string):
 ofile = open("Sersic_fit.csv", 'wb')
 writer = csv.writer(ofile)
 
-for califa_id in range(1, 940):
+for califa_id in range(1, 9):
   zpt, mag, Reff, ra, dec, ba, pa, sky, run, rerun, field, camcol, runstr, field_str, center, inputFilename = galaxyParams(califa_id)
   print califa_id, ' GALAXY ', ra, dec
-  f = pyfits.open("output/"+str(califa_id)+".fits")
+  f = pyfits.open("output/2D_"+str(califa_id)+".fits")
   y1, y1err = split_errors(f[2].header["1_YC"])
   x1, x1err = split_errors(f[2].header["1_XC"])
   ra1, dec1 = getWCSCoords(califa_id, runstr, camcol, field_str, (y1, x1))
@@ -22,6 +22,7 @@ for califa_id in range(1, 940):
   rmag1, rmag1_err = split_errors(f[2].header["1_MAG"])
   r1e, r1err = split_errors(f[2].header["1_RE"])
   n1, n1err = split_errors(f[2].header["1_N"])
+  ba1, ba1err = split_errors(f[2].header["1_AR"])
   pa1, pa1_err = split_errors(f[2].header["1_PA"])
 
   y2, y2err = split_errors(f[2].header["1_YC"])
@@ -32,17 +33,19 @@ for califa_id in range(1, 940):
   rmag2, rmag2_err = split_errors(f[2].header["2_MAG"])
   r2e, r2err = split_errors(f[2].header["2_RE"])
   n2, n2err = split_errors(f[2].header["2_N"])
+
+  ba2, ba2err = split_errors(f[2].header["2_AR"])
   pa2, pa2_err = split_errors(f[2].header["2_PA"])
 
   
-  sky, sky_err = split_errors(f[2].header["2_SKY"])
-  sky_ygrad, sky_ygrad_err = split_errors(f[2].header["2_DSDY"])
-  sky_xgrad, sky_xgrad_err = split_errors(f[2].header["2_DSDX"])
+  sky, sky_err = split_errors(f[2].header["3_SKY"])
+  sky_ygrad, sky_ygrad_err = split_errors(f[2].header["3_DSDY"])
+  sky_xgrad, sky_xgrad_err = split_errors(f[2].header["3_DSDX"])
   reduced_Chi2 = f[2].header["CHI2NU"]
   flags = f[2].header["FLAGS"]
 
 
-  data = califa_id, "CALIFA"+str(califa_id).zfill(3), ra1, ra_err1,  dec1, dec_err1, rmag1, rmag1_err, r1e, r1err, n1, n1err, pa1, pa1_err, ra2, ra_err2,  dec2, dec_err2, rmag2, rmag2_err, r2e, r2err, n2, n2err, pa2, pa2_err, sky, sky_err, sky_ygrad, sky_ygrad_err, sky_xgrad, sky_xgrad_err, reduced_Chi2, flags
+  data = califa_id, "CALIFA"+str(califa_id).zfill(3), ra1, ra_err1,  dec1, dec_err1, rmag1, rmag1_err, r1e, r1err, n1, n1err, ba1, ba1err, pa1, pa1_err, ra2, ra_err2,  dec2, dec_err2, rmag2, rmag2_err, r2e, r2err, n2, n2err, ba2, ba2err, pa2, pa2_err, sky, sky_err, sky_ygrad, sky_ygrad_err, sky_xgrad, sky_xgrad_err, reduced_Chi2, flags
 
-  writer.writerow(())
+  writer.writerow(data)
 ofile.close()  
