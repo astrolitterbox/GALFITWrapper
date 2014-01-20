63d1819a93064c76ec5841c0c5390816c8d075d7
Astrolitterbox <opit@hardcore.lt>
2014-01-20 14:43:36 +0100

    2 component fits

--- get_results.py
+++ get_results.py
@@ -7,45 +7,62 @@ def split_errors(result_string):
   return result_string.split('+/-')
 
 
-ofile = open("Sersic_fit.csv", 'wb')
-writer = csv.writer(ofile)
+listFile = 'list.txt'
+fileNames = np.genfromtxt(listFile, dtype='object')
 
-for califa_id in range(1, 9):
+def getMissingFiles(): 
+	missingFiles = []
+	for califa_id in range(1, 940):
+		if "2D_"+str(califa_id)+".fits" in fileNames:
+			print califa_id
+		else:
+			missingFiles.append(califa_id)
+
+
+#ofile = open("Sersic_fit.csv", 'w')
+#writer = csv.writer(ofile)
+fo = open("Sersic_fit.csv", "a")
+for califa_id in range(1, 940):
   zpt, mag, Reff, ra, dec, ba, pa, sky, run, rerun, field, camcol, runstr, field_str, center, inputFilename = galaxyParams(califa_id)
   print califa_id, ' GALAXY ', ra, dec
-  f = pyfits.open("output/2D_"+str(califa_id)+".fits")
-  y1, y1err = split_errors(f[2].header["1_YC"])
-  x1, x1err = split_errors(f[2].header["1_XC"])
-  ra1, dec1 = getWCSCoords(califa_id, runstr, camcol, field_str, (y1, x1))
-  ra_err1 = getAngularSize(califa_id, runstr, camcol, field_str, abs(float(y1err)))
-  dec_err1 = getAngularSize(califa_id, runstr, camcol, field_str, abs(float(x1err)))
-  rmag1, rmag1_err = split_errors(f[2].header["1_MAG"])
-  r1e, r1err = split_errors(f[2].header["1_RE"])
-  n1, n1err = split_errors(f[2].header["1_N"])
-  ba1, ba1err = split_errors(f[2].header["1_AR"])
-  pa1, pa1_err = split_errors(f[2].header["1_PA"])
+  try:
+	  f = pyfits.open("output/2D_"+str(califa_id)+".fits")
+	  y1, y1err = split_errors(f[2].header["1_YC"])
+	  x1, x1err = split_errors(f[2].header["1_XC"])
+	  ra1, dec1 = getWCSCoords(califa_id, runstr, camcol, field_str, (y1, x1))
+	  ra_err1 = getAngularSize(califa_id, runstr, camcol, field_str, abs(float(y1err)))
+	  dec_err1 = getAngularSize(califa_id, runstr, camcol, field_str, abs(float(x1err)))
+	  rmag1, rmag1_err = split_errors(f[2].header["1_MAG"])
+	  r1e, r1err = split_errors(f[2].header["1_RE"])
+	  n1, n1err = split_errors(f[2].header["1_N"])
+	  ba1, ba1err = split_errors(f[2].header["1_AR"])
+	  pa1, pa1_err = split_errors(f[2].header["1_PA"])
 
-  y2, y2err = split_errors(f[2].header["1_YC"])
-  x2, x2err = split_errors(f[2].header["1_XC"])
-  ra2, dec2 = getWCSCoords(califa_id, runstr, camcol, field_str, (y2, x2))
-  ra_err2 = getAngularSize(califa_id, runstr, camcol, field_str, abs(float(y2err)))
-  dec_err2 = getAngularSize(califa_id, runstr, camcol, field_str, abs(float(x2err)))
-  rmag2, rmag2_err = split_errors(f[2].header["2_MAG"])
-  r2e, r2err = split_errors(f[2].header["2_RE"])
-  n2, n2err = split_errors(f[2].header["2_N"])
+	  y2, y2err = split_errors(f[2].header["1_YC"])
+	  x2, x2err = split_errors(f[2].header["1_XC"])
+	  ra2, dec2 = getWCSCoords(califa_id, runstr, camcol, field_str, (y2, x2))
+	  ra_err2 = getAngularSize(califa_id, runstr, camcol, field_str, abs(float(y2err)))
+	  dec_err2 = getAngularSize(califa_id, runstr, camcol, field_str, abs(float(x2err)))
+	  rmag2, rmag2_err = split_errors(f[2].header["2_MAG"])
+	  r2e, r2err = split_errors(f[2].header["2_RE"])
+	  n2, n2err = split_errors(f[2].header["2_N"])
 
-  ba2, ba2err = split_errors(f[2].header["2_AR"])
-  pa2, pa2_err = split_errors(f[2].header["2_PA"])
+	  ba2, ba2err = split_errors(f[2].header["2_AR"])
+	  pa2, pa2_err = split_errors(f[2].header["2_PA"])
 
   
-  sky, sky_err = split_errors(f[2].header["3_SKY"])
-  sky_ygrad, sky_ygrad_err = split_errors(f[2].header["3_DSDY"])
-  sky_xgrad, sky_xgrad_err = split_errors(f[2].header["3_DSDX"])
-  reduced_Chi2 = f[2].header["CHI2NU"]
-  flags = f[2].header["FLAGS"]
+	  sky, sky_err = split_errors(f[2].header["3_SKY"])
+	  sky_ygrad, sky_ygrad_err = split_errors(f[2].header["3_DSDY"])
+	  sky_xgrad, sky_xgrad_err = split_errors(f[2].header["3_DSDX"])
+	  reduced_Chi2 = f[2].header["CHI2NU"]
+	  flags = f[2].header["FLAGS"]
 
 
-  data = califa_id, "CALIFA"+str(califa_id).zfill(3), ra1, ra_err1,  dec1, dec_err1, rmag1, rmag1_err, r1e, r1err, n1, n1err, ba1, ba1err, pa1, pa1_err, ra2, ra_err2,  dec2, dec_err2, rmag2, rmag2_err, r2e, r2err, n2, n2err, ba2, ba2err, pa2, pa2_err, sky, sky_err, sky_ygrad, sky_ygrad_err, sky_xgrad, sky_xgrad_err, reduced_Chi2, flags
-
-  writer.writerow(data)
-ofile.close()  
+	  data = str(califa_id)+","+ "CALIFA"+str(califa_id).zfill(3)+", "+ str(ra1)+","+str(ra_err1)+","+str(dec1)+","+str(dec_err1)+","+str(rmag1)+","+ str(rmag1_err)+","+ str(r1e)+","+str(r1err)+","+ str(n1)+","+ str(n1err)+","+str(ba1)+","+str(ba1err)+","+str(pa1)+","+str(pa1_err)+","+ str(ra2)+","+ str(ra_err2)+","+ str(dec2)+","+str(dec_err2)+","+ str(rmag2)+","+ str(rmag2_err)+","+ str(r2e)+","+str(r2err)+","+ str(n2)+","+ str(n2err)+","+ str(ba2)+","+ str(ba2err)+","+ str(pa2)+","+ str(pa2_err)+","+ str(sky)+","+ str(sky_err)+","+str(sky_ygrad)+","+ str(sky_ygrad_err)+","+ str(sky_xgrad)+","+ str(sky_xgrad_err)+","+ str(reduced_Chi2)+","+ str(flags)+"\n"
+	  fo.write(data)
+  except (IOError, ValueError):
+	  data = califa_id
+	  fo.write(str(data)+"-999\n")
+  
+  #writer.writerow(data)
+fo.close()  
--- input/run_galfit.py
+++ input/run_galfit.py
@@ -13,13 +13,14 @@ def doFitting(FileList):
 
 def main():
 
-  FileList0 = range(23, 188)
-  FileList1 = range(304, 376)
-  FileList2 = range(486, 564)
-  FileList3 = range(679, 752)
-  FileList4 = range(861, 940)
+  #FileList0 = range(112, 121)
+  #FileList1 = range(130, 138)
+  FileList1 = [81, 144, 147, 338, 43]
+  FileList2 = [491, 505, 540, 652, 884]
+  #FileList3 = range(160, 170)
+  #FileList4 = range(178, 188)
 
-  for fileList in [FileList0, FileList1, FileList2, FileList3, FileList4]:
+  for fileList in [FileList2, FileList1]:#a, FileList1, FileList2, FileList3, FileList4]:
     print 'filelist', fileList
     p = multiprocessing.Process(target=doFitting, args=[fileList])
     p.start()
